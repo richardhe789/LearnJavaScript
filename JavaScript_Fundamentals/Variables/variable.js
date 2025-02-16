4bf4365aec01fb2bb890c243e5718e0171b853ff
Richard He <132486430+richardhe789@users.noreply.github.com>
2025-02-16 15:01:36 -0500

    new notes

---
+++ JavaScript_Fundamentals/Variables/variable.js
@@ -0,0 +1,43 @@
+// let admin, name2;
+// name2 = "John";
+// admin = name2;
+// alert(admin);
+
+// let planetName = "Earth";
+// let userName = "Millie";
+
+// it would be right to use upper case for birthday only because its the only value that never changes, age must stay lowercase.
+//-------------
+//hello 1
+//hello name
+//hello Ilya
+
+// let age = prompt('How old are you?', 100); // 100 makes it pre-filled in
+// alert(`You are ${age} years old!`);
+
+// alert = notification
+// prompt = type an answer (like Scanner in Java)
+// confirm = bool (yes/no)
+
+// let name = prompt("What is your name?", '');
+// alert(`Your name is ${name}!`);
+
+// conversion mainly works with casting , String(value) or Number(string) - for number specifically just add "+" before the string
+// uranary is PEMDAS applied to a single variable, binary is PEMDAS applied to two
+// equals sign performs the assignment and returns the result when used in an expression
+// increment + decrement similar to java (++, --)
+// "--" and "++" specifically return different things based on whether they are before/after the variable
+    // if before, returns changed value, if after, returns unchanged value
+    // just writing an expression like "c = ++a" changes a
+// weird comma operators can perform multiple calculations at once but only keep the result of the last one
+
+// a = 2, b = 2, c = 2 (before returns changed value), d = 1 (after returns unchanged value)
+// a = 4 (changed DURING the assignment of x), x = 5
+
+// 10, -1, 1, 2, 6, 9px, $45, 2, NaN, -9  5, -14, 1, NaN, -2
+// subtraction always converts to numbers!!
+// null turns into 0 after numeric conversion and undefined becomes NaN
+
+//Both numbers are being taken in as strings, you can use alert(+a + +b); or Number casting
+
+// === is used to check equality without type conversion.
